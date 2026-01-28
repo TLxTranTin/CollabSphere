546a63761f3f49278234fadd9e9aacd0725d7293
TinCa <nghialt7265@ut.edu.vn>
2026-01-28 07:57:11 +0700

    Add MonHoc parameter to BaiKiemTra constructor

--- temp-clean-arch/src/domain/models/Bai_Kiem_Tra/Bai_Kiem_Tra.py
+++ temp-clean-arch/src/domain/models/Bai_Kiem_Tra/Bai_Kiem_Tra.py
@@ -1,5 +1,7 @@
+from domain.models.Mon_Hoc.Mon_Hoc import MonHoc
 class BaiKiemTra:
-    def __init__(self, id: str, ten: str, mo_ta: str):
+    def __init__(self, id: str, ten: str, mo_ta: str , mon_hoc : MonHoc):
         self.id = id
         self.ten = ten
         self.mo_ta = mo_ta
+        self.mon_hoc = mon_hoc
